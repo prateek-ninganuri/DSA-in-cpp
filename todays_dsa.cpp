d684170dc298bd125f5028720c1cd5f07ddd6a55
prateek-ninganuri <prateeksn2007@gmail.com>
2026-05-24 20:47:13 +0530

    Add files via upload

---
+++ todays_dsa.cpp
@@ -0,0 +1,22 @@
+//reverse integer//
+
+class Solution {
+public:
+    int reverse(int x) {
+        int sum=0;
+        while(x!=0){
+            //overflow check
+            if(sum > INT_MAX/10 || sum < INT_MIN/10 ){
+                    return 0;
+            }else{
+                  sum=sum*10+x%10;
+                    x=x/10;
+             
+               
+            }
+           
+        }
+        
+        return sum;
+    }
+};
